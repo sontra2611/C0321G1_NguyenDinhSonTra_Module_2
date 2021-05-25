b08b252729bce0b9a1531119461828bdac4b23e9
TechCare <sontra9798@gmail.com>
2021-05-25 16:31:05 +0700

    Up bài tập và thực hành

--- src/bai_15_xu_ly_ngoai_le_va_debug/bai_tap/IllegalTriangleException.java
+++ src/bai_15_xu_ly_ngoai_le_va_debug/bai_tap/IllegalTriangleException.java
@@ -1,7 +1,5 @@
 package bai_15_xu_ly_ngoai_le_va_debug.bai_tap;
 
-import java.util.Scanner;
-
 public class IllegalTriangleException extends Exception {
     public IllegalTriangleException(String s) {
         super(s);
