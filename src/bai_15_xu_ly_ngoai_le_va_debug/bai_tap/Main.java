0354f89c0e9817469c355fe2f607e8476b0b190f
TechCare <sontra9798@gmail.com>
2021-05-25 16:27:38 +0700

    Up bài tập và thực hành

---
+++ src/bai_15_xu_ly_ngoai_le_va_debug/bai_tap/Main.java
@@ -0,0 +1,30 @@
+package bai_15_xu_ly_ngoai_le_va_debug.bai_tap;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean check = true;
+        do {
+            try {
+                System.out.println("Nhập vào 3 cạnh của tam giác : ");
+                System.out.println("Nhập canh thứ nhất :");
+                int size1 = Integer.parseInt(scanner.nextLine());
+                System.out.println("Nhập canh thứ hai :");
+                int size2 = Integer.parseInt(scanner.nextLine());
+                System.out.println("Nhập canh thứ ba :");
+                int size3 = Integer.parseInt(scanner.nextLine());
+                Triangle triangle = new Triangle(size1, size2, size3);
+                System.out.println(triangle.toString());
+                check = false;
+            }catch (NumberFormatException e){
+                System.err.println("Bạn nhập không hợp lệ, vui lòng nhập số");
+                check =true;
+            }catch (IllegalTriangleException e) {
+                System.err.println(e);
+                check = true;
+            }
+        }while (check);
+    }
+}
--- src/bai_15_xu_ly_ngoai_le_va_debug/bai_tap/Triangle.java
+++ src/bai_15_xu_ly_ngoai_le_va_debug/bai_tap/Triangle.java
@@ -1,5 +1,8 @@
 package bai_15_xu_ly_ngoai_le_va_debug.bai_tap;
 
+import jdk.internal.util.xml.impl.Input;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Triangle {
@@ -7,10 +10,12 @@ public class Triangle {
     private double size2;
     private double size3;
 
-    public Triangle(double size1, double size2, double size3) throws IllegalTriangleException{
-        if(size1+size2<size3 || size1+size3<size2 || size2+size3<size1 || size1 < 0 || size2 <0 || size3 < 0)
-            throw new IllegalTriangleException("Đây không phải tam giác");
-        else{
+    public Triangle(double size1, double size2, double size3) throws IllegalTriangleException,NumberFormatException {
+        if (size1 <= 0 || size2 <= 0 || size3 <= 0) {
+            throw new IllegalTriangleException("Không phải tam giác vì cạnh < 0");
+        } else if (size1 + size2 <= size3 || size1 + size3 <= size2 || size2 + size3 <= size1) {
+            throw new IllegalTriangleException("Không phải tam giác vì tổng 2 cạnh bé hơn cạnh còn lại");
+        } else {
             this.size1 = size1;
             this.size2 = size2;
             this.size3 = size3;
@@ -24,24 +29,5 @@ public class Triangle {
                 ", size2=" + size2 +
                 ", size3=" + size3 +
                 '}';
-    }
-
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Nhập vào 3 cạnh của tam giác : ");
-        System.out.println("Nhập canh thứ nhất :");
-        int size1 = scanner.nextInt();
-        System.out.println("Nhập canh thứ hai :");
-        int size2 = scanner.nextInt();
-        System.out.println("Nhập canh thứ ba :");
-        int size3 = scanner.nextInt();
-
-        try {
-            Triangle triangle = new Triangle(size1, size2, size3);
-            System.out.println(triangle.toString());
-        }
-        catch (IllegalTriangleException e){
-            System.out.println(e);
-        }
     }
 }
